3548e7cd5f848887e806d1da8901fbee9e4f0e8e
LiYanLance <leeyanly@163.com>
2018-04-11 18:16:24 +0800

    return Fizz when multiples 3

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -2,13 +2,20 @@ package com.thoughtworks;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
     public void start(int count) {
-
+        for (int i = 1; i <= count; i++) {
+            String result = null;
+            if(i % 3 == 0){
+                result = "Fizz";
+            }
+            results.add(Optional.ofNullable(result).orElse(String.valueOf(i)));
+        }
     }
 
     public List<String> getResults() {
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,9 +2,22 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
-public class FizzBuzzGameTest {
-    @Test
-    public void should_return_Fizz_when_have_3() {
+import java.util.List;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FizzBuzzGameTest {
+
+    private FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+    private List<String> results = fizzBuzzGame.getResults();
+
+    @Test
+    public void should_return_Fizz_when_multiples_3() {
+        fizzBuzzGame.start(100);
+        results = fizzBuzzGame.getResults();
+
+        assertThat(results.get(2), is("Fizz"));
+        assertThat(results.get(5), is("Fizz"));
     }
 }
